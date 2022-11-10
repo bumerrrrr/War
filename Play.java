1b6c3bf1441a057a85bd2d47f2780115b3d04c2b
ShenG109 <117760856+ShenG109@users.noreply.github.com>
2022-11-10 09:30:33 -0600

    Update Play.java

--- Play.java
+++ Play.java
@@ -23,6 +23,46 @@ import java.util.ArrayList;
     
     public String playGame(Deck game, ArrayList<Card> player1, ArrayList<Card> player2)
         {
-        //play the game
+        int startGame = 0;
+        while(startGame < 1)
+          {
+          int smallestHand = player1.size();
+          if(player2.size() < smallestHand)
+            smallestHand = player2.size();
+          int i = 0;
+          while(i < smallestHand)
+            {
+            Card p1Card = player1.remove(i);
+            Card p2Card = player2.remove(i);
+            int p1Num = p1card.getValue();
+            int p2Num = p2Card.getValue();
+            if(p1Num > p2Num)
+              {
+              player1.add(p1card);
+              player1.add(p2card);
+              }
+            if(p2num > p1Num)
+              {
+              player2.add(p1card);
+              player2.add(p1card);
+              }
+//tie val
+            
+            if(p1Num == p2Num)
+              {
+              boolean equal = true;
+              ArrayList<Card> overTime1 = new ArrayList<Card>();
+               ArrayList<Card> overTime2 = new ArrayList<Card>();
+              while(equal == true)
+               {
+               
+//think i have to add ArrayList to stack
+              Card p1Cover = player1.remove(i);
+              Card p2Cover = player2.remove(i);
+              Card p1Show = player1.remove(i);
+              Card p2Show = player2.remove(i);
+              }
+            }
+           
+          }
         }
-    }
