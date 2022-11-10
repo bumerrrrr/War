2216a7bd5bcdbeda48909d74396aaa2574c996de
ShenG109 <117760856+ShenG109@users.noreply.github.com>
2022-11-10 08:49:08 -0600

    Create Play.java

---
+++ Play.java
@@ -0,0 +1,28 @@
+/**
+ * P-1
+ * Description of the project
+ *
+ * @author Shen Groppel
+ * @version 9-1-22
+ */
+
+import java.util.ArrayList;
+
+ public class Play
+    {
+    private ArrayList<Card> player1
+    private ArrayList<Card> player2
+
+/**
+*/
+    public Play(ArrayList<Card> p1, ArrayList<Card> p2)
+        {
+         player1 = p1;
+         player2 = p2;
+        }
+    
+    public String playGame(Deck game, ArrayList<Card> player1, ArrayList<Card> player2)
+        {
+        //play the game
+        }
+    }
