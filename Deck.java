20d52b33b250a06ca337b9fba7cb7ae9ee282e05
littlelingard <91565258+littlelingard@users.noreply.github.com>
2022-11-10 09:01:34 -0600

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -4,13 +4,33 @@ import java.util.ArrayList;
     private ArrayList<Card> deck;
     private int topCard;
   
-  
-
-  
-    public void shuffle() 
-    {
-   
+    public void shuffle()
+     {
+     String tempSuit = "";
+     for (int i = 0; i < 4; i++)
+      {
+      if (i == 0)
+       {
+       tempSuit = "Hearts";
+       }
+      if (i == 1)
+       {
+       tempSuit = "Spades";
+       }
+    if (i == 2)
+      {
+      tempSuit = "Diamonds";
+      }
+    if (i == 3)
+      {
+      tempSuit = "Clubs";
+      }
+    for (int x = 2; x <= 14; x++)
+      {
+      Deck.add(new Card(i, tempSuit));
+      }
     }
+  }
    
   
     public void dealCard(ArrayList<Card>player1, ArrayList<Card>player2)
