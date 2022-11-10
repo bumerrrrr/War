7a4725b3d8a5c7a28bbe7748a8d50830d4a37ce6
bumerrrrr <91628482+bumerrrrr@users.noreply.github.com>
2022-11-09 23:27:40 -0600

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -4,5 +4,25 @@ import java.util.ArrayList;
     private Card[] deck
     private int topCard;
     
+    deck()
+    {
+    topCard = 0;
+    for (int i = 0; i < deck.length; i++)
+            deck[i] = new Card(i);
+    }
+    
+    public void shuffle() 
+    {
+     topCard = 0;
+
+     for (int i = 0; i < 10000; i++) 
+         {
+         int p1 = (int)(Math.random()*52);
+         int p2 = (int)(Math.random()*52);
+         Card temp = deck[j];
+         deck[p1] = deck[p2];
+         deck[p2] = tmpCard;
+         }
+    
     
     }
